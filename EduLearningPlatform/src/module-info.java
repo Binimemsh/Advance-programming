537235c40ae25554585aa9e517de509103fe7398
Biniam <biniamemshaw84@gmail.com>
2025-12-02 10:49:31 +0300

    almost done

--- EduLearningPlatform/src/module-info.java
+++ EduLearningPlatform/src/module-info.java
@@ -1,11 +1,16 @@
 module EduLearningPlatform {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires jdk.jdi;
-	requires java.sql;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	exports application;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires javafx.base;
+    requires java.desktop;
+    requires java.sql;
+    // Add PDFBox modules
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.io;
+	//requires javafx.swing;
+    
+    opens application to javafx.fxml, javafx.graphics;
+    exports application;
 }
